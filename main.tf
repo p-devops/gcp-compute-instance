98fd45eaa40f8df3848441c09c735af39a5a756c
Peter Leung <pleung@hashicorp.com>
2020-10-21 12:06:58 -0400

    First commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 0.11.1"
+}
+
+provider "google" {
+  //credentials = "${var.gcp_credentials}"
+  //project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
+resource "google_compute_instance" "demo" {
+  name         = "${var.instance_name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.gcp_zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+}
+
+output "external_ip"{
+  value = "${google_compute_instance.demo.network_interface.0.access_config.0.nat_ip}"
+}
---
+++ policies/sentinel.hcl
@@ -0,0 +1,7 @@
+policy "gcp-block-allow-all-cidr" {
+    enforcement_level = "hard-mandatory"
+}
+
+policy "gcp-restrict-machine-type" {
+    enforcement_level = "soft-mandatory"
+}
