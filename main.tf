1acc55e215c8b50ae33a3d3039b0a39647a341f0
peter leung <49036546+p-devops@users.noreply.github.com>
2020-10-21 12:20:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,42 @@ terraform {
   required_version = ">= 0.11.1"
 }
 
+variable "gcp_credentials" {
+  description = "GCP credentials needed by google provider"
+}
+
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "gcp_region" {
+  description = "GCP region, e.g. us-east1"
+  default = "us-east1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone, e.g. us-east1-a"
+  default = "us-east1-b"
+}
+
+variable "machine_type" {
+  description = "GCP machine type"
+  default = "n1-standard-1"
+}
+
+variable "instance_name" {
+  description = "GCP instance name"
+  default = "demo"
+}
+
+variable "image" {
+  description = "image to build instance from"
+  default = "debian-cloud/debian-9"
+}
+
 provider "google" {
-  //credentials = "${var.gcp_credentials}"
-  //project     = "${var.gcp_project}"
+  credentials = "${var.gcp_credentials}"
+  project     = "${var.gcp_project}"
   region      = "${var.gcp_region}"
 }
 
